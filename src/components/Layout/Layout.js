946c031d6527d58acc8ea0f3c9184354fcfd57fb
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-07 23:29:01 +0100

    added nav bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
-
-
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import "./Layout.css"
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, sideDrawer, Backdrop</div>
+        <Toolbar/>
         <main className="Content">{props.children}</main>
     </Aux>
 )

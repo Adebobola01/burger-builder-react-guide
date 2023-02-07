f17b6e4de47c6fa5a86a2029602e9fbfd4e4a4a8
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-07 23:41:55 +0100

    added logo comp

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Toolbar.css"
-
+import Logo from "../../Logo/Logo";
 const toolbar = (props) => (
     <header className="Toolbar" >
         <div>menu</div>
-        <div>logo</div>
+        <Logo/>
         <nav>...</nav>
     </header>
 )

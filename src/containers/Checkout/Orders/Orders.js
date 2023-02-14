f5b621d5095a1b9c2c4c3eb1292680400ed6898f
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-14 23:58:30 +0100

    fixed orders page

--- src/containers/Checkout/Orders/Orders.js
+++ src/containers/Checkout/Orders/Orders.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Order from "../../../components/Order/Order/Order";
+
 
 class Orders extends Component{
     render() {
         return (
             <div>
-
+                <p>ingredients: Salad(1)</p>
+                <p>Price: USD 5.45</p>
             </div>
         )
     }

0011d9d6c2e0994344a66284393242608237f974
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-14 23:54:36 +0100

    added order

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Checkout/Orders/Orders';
 // import ContactData from './containers/Checkout/ContactData/ContactData';
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       <div>
         <Layout>
           <Routes >
-            <Route path='/checkout/*' element={<Checkout/>}/>
+            <Route path='/checkout/*' element={<Checkout />} />
+            <Route path='/orders' element={<Orders/>} />
             <Route path='/' element={<BurgerBuilder />} />
             {/* <Route path="checkout/contact-data" element={<ContactData/>}/> */}
           </Routes>

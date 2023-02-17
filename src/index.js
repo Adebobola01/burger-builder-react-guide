1fe7c362edd64c5c7312864fe960ed2d55dbbdc1
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-17 23:05:22 +0100

    added store and reducer

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-
+import { Provider } from 'react';
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './store/reducer';
 import { createBrowserHistory } from "history";
 const history = createBrowserHistory({forceRefresh: true});
 
+const store = createStore(reducer);
+
 const app = (
-    <BrowserRouter history={history} >
-        <App/>
-    </BrowserRouter>
+    <Provide store={store} >
+        <BrowserRouter history={history} >
+            <App/>
+        </BrowserRouter>
+    </Provide>
 )
 
 ReactDOM.render(app, document.getElementById('root'));

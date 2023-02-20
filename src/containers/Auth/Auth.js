a8d51700d51696571ee1a458cdc4bae492d99810
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-20 23:46:52 +0100

    added spinner

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import Spinner from "../../components/UI/Spinner/Spinner"
 import "./Auth.css"
+// import resolve from "resolve";
+// import { reject } from "lodash";
 class Auth extends Component {
 
     state = {
@@ -9,7 +12,7 @@ class Auth extends Component {
             email: {
                 elementType: "input",
                 elementConfig: {
-                    type: "text",
+                    type: "email",
                     placeholder: "your email"
                 },
                 value: ""
@@ -22,24 +25,68 @@ class Auth extends Component {
                 },
                 value: ""
             }
-        }
+        },
+        auth: {
+            token: "",
+            exp: "34"
+        },
+        loading: false
+    }
+    inputChangeHandler = (e, inputIdentifier) => {
+        const updatedAuthForm = { ...this.state.controls };
+        const updatedFormEl = updatedAuthForm[inputIdentifier];
+        updatedFormEl.value = e.target.value;
+        updatedAuthForm[inputIdentifier] = updatedFormEl;
+        this.setState({ controls: updatedAuthForm });
+    }
+
+    submitHandler = async(e) => {
+        e.preventDefault();
+        this.setState({loading: true})
+        const data = await new Promise((resolve, reject) => {       
+            setTimeout(() => {
+                const data = {
+                    idToken: "lkafdlksjflkajflkjaflk;flkas",
+                }
+                this.setState({loading: false})
+                resolve(data)
+            }, 3000)
+        })
+        this.setState((prevState) => {
+            return {
+                auth: {
+                    ...prevState.auth,
+                    token: data.idToken
+                }
+            }
+        })
+        console.log(this.state)
     }
 
     render() {
 
         const formEl = Object.keys(this.state.controls).map(key => {
-            return this.state.controls[key]
+            return {
+                ...this.state.controls[key],
+                key: key
+            }
         })
-        console.log(formEl)
-        
-        
-        return (
-            <div>
-                <form className="AuthData" >
-                    {formEl.map((el, i) => (
-                        <Input key={i} elementType={el.elementType} elementConfig={el.elementConfig} />
-                    ))}
+
+        let form = (
+            formEl.map((el, i) => (
+                <Input key={i} elementType={el.elementType} elementConfig={el.elementConfig} changed={(e) => this.inputChangeHandler(e, el.key)} />
+            ))
+        )
+            
+            
+            return (
+                <div>
+                <form className="AuthData" onSubmit={this.submitHandler} >
+                    {this.state.loading ? <Spinner /> :
+                        form
+                    }
                     <Button btnType="Success">Submit</Button>
+
                 </form>
             </div>
         )

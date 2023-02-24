705538e5e0214e40e758b09c23bbe9687d5ccb28
Adebobola01 <m.oyedunmade@gmail.com>
2023-02-24 23:06:57 +0100

    added fallback to routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Spinner from './components/UI/Spinner/Spinner';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 const Checkout = lazy(()=> import('./containers/Checkout/Checkout'));
 const Orders = lazy(()=> import('./containers/Checkout/Orders/Orders'));
 const Auth = lazy(()=> './containers/Auth/Auth');
 // import ContactData from './containers/Checkout/ContactData/ContactData';
 const app = () => {
+    let routes = (
+            <Routes >
+              <Route path='/checkout/*' element={ <Suspense fallback={<Spinner/>} > <Checkout /></Suspense>} />
+              <Route path='/orders' element={<Suspense fallback={<Spinner/>}> <Orders /></Suspense>} />
+              <Route path='/' index element={<Suspense fallback={<Spinner/>}> <BurgerBuilder /></Suspense>} />
+              <Route path="/auth" element={<Suspense fallback={<Spinner/>}> <Auth /></Suspense>}/>
+            </Routes>     
+    )
+
     return (
       <>
         <Layout>
-          <Suspense>
-            <Routes >
-              <Route path='/checkout/*' element={ <Suspense> <Checkout /></Suspense>} />
-              <Route path='/orders' element={<Suspense> <Orders /></Suspense>} />
-              <Route path='/' index element={<Suspense> <BurgerBuilder /></Suspense>} />
-              <Route path="/auth" element={<Suspense> <Auth /></Suspense>}/>
-            </Routes>
+          <Suspense fallback={<Spinner/>}>
+            {routes}
           </Suspense>
         </Layout>
       </>
